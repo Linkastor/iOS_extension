74f4960035f6f91668de2c5747a54bde82787400
Thibaut LE LEVIER <thibaut@lelevier.fr>
2015-06-29 16:13:30 +0200

    initial commit

---
+++ Linkastor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Linkastor
+//
+//  Created by Thibaut LE LEVIER on 29/06/2015.
+//  Copyright © 2015 Thibaut LE LEVIER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
